14b445251abd098b409c6f1ceda184f6f4bfde7f
Pavel Fedarovich <fedorovich.pavel@mail.ru>
2020-10-01 00:05:25 +0300

    feat:add js code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined) { return 0; } else {
+        arr1 = array.sort((a, b) => a - b);
+        return arr1[0] || 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined) { return 0; } else {
+        arr1 = array.sort((a, b) => a - b);
+        return arr1[arr1.length - 1] || 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined) { return 0; } else {
+        var sum = 0;
+        array.forEach(element => {
+            sum += element;
+        });
+        return sum / (array.length) || 0;
+    }
 }
